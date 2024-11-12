df9ffcf7a9d1fc74ed892491e37156e3c19ebaf6
Daniel Peshkov <109922163+DanielPeshkov@users.noreply.github.com>
2024-11-12 15:01:39 -0500

    Backend Beginnings

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+import pkg from "express";
+const { express } = pkg;
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send({'response': 'Server Response!'});
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
